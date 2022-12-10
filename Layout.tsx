6bf63f238ac401def9f7d914f5aaaef2de8c3993
Yousef Shanawany <askyous@gmail.com>
2022-12-10 07:50:15 -0800

    Sidenav

--- Layout.tsx
+++ Layout.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { AppUsers } from "./database/entities/AppUsers";
 import { Footer } from "./components/Footer";
-import { Navbar } from "./components/Navbar";
+import { SideNav } from "./components/forms/SideNav/SideNav";
 import { UserContext } from "./utils/UserContext";
 
 interface Props {
@@ -16,38 +16,39 @@ export const Layout = ({ children }: Props) => {
             if (r.status == 404) return;
             const me = await r.json();
             setUser(me);
-        })
+        });
     }, []);
 
-
-    return <UserContext.Provider value={user}>
-        <Navbar
-            onRegisterRequest={async (userNameAndPassword) => {
-                const res = await fetch("/api/register", {
-                    method: "POST",
-                    body: JSON.stringify(userNameAndPassword),
-                });
-                if (res.status != 200) return window.alert(await res.text());
-                const registeredUser = await res.json();
-                setUser(registeredUser);
-            }}
-            onLogoutRequest={async () => {
-                await fetch("/api/logout");
-                setUser(undefined);
-            }}
-            onLoginRequest={({ userName, password }) => {
-                fetch("/api/login", {
-                    method: "POST",
-                    body: JSON.stringify({ userName, password }),
-                })
-                    .then(async r => {
-                        if (r.status !== 200) return window.alert(await r.text());
-                        setUser(await r.json() as AppUsers);
-                    });
-            }} />
-        <div style={{ padding: "0 1rem", maxWidth: "1024px", margin: "0 auto" }}>
-            {children}
-        </div>
-        <Footer />
-    </UserContext.Provider>;
+    return (
+        <UserContext.Provider value={user}>
+            <div style={{ display: "flex", justifyContent: "flex-start", padding: "1rem" }}>
+                <SideNav
+                    onRegisterRequest={async userNameAndPassword => {
+                        const res = await fetch("/api/register", {
+                            method: "POST",
+                            body: JSON.stringify(userNameAndPassword),
+                        });
+                        if (res.status != 200) return window.alert(await res.text());
+                        const registeredUser = await res.json();
+                        setUser(registeredUser);
+                    }}
+                    onLogoutRequest={async () => {
+                        await fetch("/api/logout");
+                        setUser(undefined);
+                    }}
+                    onLoginRequest={({ userName, password }) => {
+                        fetch("/api/login", {
+                            method: "POST",
+                            body: JSON.stringify({ userName, password }),
+                        }).then(async r => {
+                            if (r.status !== 200) return window.alert(await r.text());
+                            setUser((await r.json()) as AppUsers);
+                        });
+                    }}
+                />
+                <div style={{ flex: "1" }}>{children}</div>
+            </div>
+            <Footer />
+        </UserContext.Provider>
+    );
 };
---
+++ components/forms/SideNav/SideNav.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { useContext, useState } from "react";
+import { UserContext } from "../../../utils/UserContext";
+import { AuthDialog } from "../../AuthDialog";
+import { NavLink } from "./NavLink/NavLink";
+import styles from "./style.module.css";
+
+interface Credentials {
+    userName: string;
+    password: string;
+}
+
+interface Props {
+    onLoginRequest: (credentials: Credentials) => void;
+    onRegisterRequest: (credentials: Credentials) => void;
+    onLogoutRequest: VoidFunction;
+}
+
+export const SideNav = ({ onLoginRequest, onRegisterRequest, onLogoutRequest }: Props) => {
+    const [authState, setAuthState] = useState<"logging-in" | "registering">();
+    return (
+        <div className={styles.sidenav}>
+            <NavLink href="/" text="Islaam Database" icon="🖥" />
+            <NavLink href="/people" text="People" icon="👥" />
+            <NavLink href="/praises" text="Praises" icon="👍" />
+            <NavLink href="/students" text="Students" icon="🏫" />
+            <div style={{ display: "flex", gap: "1rem" }}>
+                <AuthDialog
+                    isOpen={authState != null}
+                    onCloseRequest={() => setAuthState(undefined)}
+                    submitText={authState == "logging-in" ? "Log in" : "Register"}
+                    onSubmit={authState === "logging-in" ? onLoginRequest : onRegisterRequest}
+                />
+                <UserLogin
+                    onLoginRequest={() => setAuthState("logging-in")}
+                    onRegisterRequest={() => setAuthState("registering")}
+                    onLogoutRequest={onLogoutRequest}
+                />
+            </div>
+        </div>
+    );
+};
+
+interface UserLoginProps {
+    onLoginRequest: VoidFunction;
+    onRegisterRequest: VoidFunction;
+    onLogoutRequest: VoidFunction;
+}
+
+function UserLogin({ onLoginRequest, onRegisterRequest, onLogoutRequest }: UserLoginProps) {
+    const user = useContext(UserContext);
+    if (user) return <button onClick={onLogoutRequest}>Assalaamu 'Alaikum, {user.userName}!</button>;
+    return (
+        <>
+            <button onClick={onLoginRequest}>Login</button>
+            <button onClick={onRegisterRequest}>Register</button>
+        </>
+    );
+}
