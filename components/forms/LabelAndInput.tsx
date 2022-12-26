6162baea4a5640d92609b8484675692b33fbbdfe
Yousef Shanawany <askyous@gmail.com>
2022-12-26 14:58:03 -0800

    form fields styling

--- components/forms/LabelAndInput.tsx
+++ components/forms/LabelAndInput.tsx
@@ -5,7 +5,10 @@ interface Props extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
 }
 
 export const LabelAndInput = (p: Props) => (
-    <div>
-        <label htmlFor={p.id}>{p.label}</label> <input {...p} />
+    <div style={{ display: "flex", gap: ".5rem", marginBottom: ".5rem", alignItems: "center" }}>
+        <label style={{ fontWeight: p.required ? "bold" : "unset" }} htmlFor={p.id}>
+            {p.label}
+        </label>
+        <input style={{ flex: 1, padding: ".5rem" }} {...p} />
     </div>
 );
